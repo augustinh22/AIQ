97c29576243112cab9b39807ea39dd837f617863
augustinh22 <hannah.augustin@gmail.com>
2018-01-18 10:55:25 +0100

    automate ingestion

--- thesis/ingestion/automate_ODC.sh
+++ thesis/ingestion/automate_ODC.sh
@@ -3,5 +3,15 @@
 source ~/Datacube/datacube_env/bin/activate
 
 cd /home/odci/Datacube/agdc-v2/ingest/prepare_scripts/sentinel_2/
+
 python prep_s2.py
 python index_s2.py
+
+cd /home/odci/Datacube/agdc-v2/ingest/prepare_scripts/siam/
+
+python prep_siam.py
+python index_siam.py
+
+datacube -v ingest -c /home/odci/Datacube/agdc-v2/ingest/ingestion_configs/siam/s2_siam_epsg32637_syria.yaml --executor multiproc 10
+
+datacube -v ingest -c /home/odci/Datacube/agdc-v2/ingest/ingestion_configs/siam/s2_siam_epsg32637_syria_25km.yaml --executor multiproc 10
