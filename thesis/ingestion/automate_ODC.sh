ddabecef44e8a9b98489c97a3aea32111d2ffc3e
Hannah <hannah.augustin@gmail.com>
2018-01-25 14:31:59 +0100

    Update automate_ODC.sh

--- thesis/ingestion/automate_ODC.sh
+++ thesis/ingestion/automate_ODC.sh
@@ -12,6 +12,4 @@ cd /home/odci/Datacube/agdc-v2/ingest/prepare_scripts/siam/
 python prep_siam.py
 python index_siam.py
 
-datacube -v ingest -c /home/odci/Datacube/agdc-v2/ingest/ingestion_configs/siam/s2_siam_epsg32637_syria.yaml --executor multiproc 10
-
-datacube -v ingest -c /home/odci/Datacube/agdc-v2/ingest/ingestion_configs/siam/s2_siam_epsg32637_syria_25km.yaml --executor multiproc 10
+datacube -v ingest -c /home/odci/Datacube/agdc-v2/ingest/ingestion_configs/siam/s2_siam_epsg32637_syria_100km.yaml --executor multiproc 10
