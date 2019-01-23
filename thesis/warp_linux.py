615f2561489a1ab5cd694e8a572294611c585b24
Hannah <hannah.augustin@gmail.com>
2019-01-23 11:46:20 +0100

    test files to warp

--- thesis/warp_linux.py
+++ thesis/warp_linux.py
@@ -512,167 +512,69 @@ def get_procFolders(options_in):
 
     return bool_ans, unprocFolders
 
-def warp_envi(root_folder, imgFolders, warp_epsg):
+
+def warp_envi(root_folder, procFolders, warp_epsg):
 
     start_time = datetime.datetime.now()
 
     print '================================================================='
     print 'Hold on to your hat. This may take ~45s per S2 tile folder.'
     print 'Number of unprocessed PROC_DATA folders found: {}'.format(
-        len(imgFolders))
-    print 'Estimated time: {} minutes'.format(int(len(imgFolders)) * 0.75)
+        len(procFolders))
+    print 'Estimated time: {} minutes'.format(int(len(procFolders)) * 0.75)
     print 'Start time: {}'.format(start_time.time())
     print '=================================================================\n'
 
     message = (
         'Root Folder: {} \nNumber of unprocessed IMG_DATA folders '
         'found: {}\nStart time: {}'
-        ).format(root_folder, len(imgFolders), start_time.time())
+        ).format(root_folder, len(procFolders), start_time.time())
     logger.info(message)
     #
     # Register all of the GDAL drivers.
     #
     gdal.AllRegister()
 
-    #
-    # Possible XML Schema namespaces (plus a few potential future ones.)
-    #
-    XML_namespaces = ['https://psd-14.sentinel2.eo.esa.int/',
-                      'https://psd-12.sentinel2.eo.esa.int/',
-                      'https://psd-13.sentinel2.eo.esa.int/',
-                      'https://psd-15.sentinel2.eo.esa.int/',
-                      'https://psd-16.sentinel2.eo.esa.int/']
-
-    i = 0
-
-    for imgFolder in imgFolders:
-
-        metadata_path = []
-
-        for fn in os.listdir(os.path.dirname(imgFolder)):
-            if ((fn.startswith('S2A_') or fn.startswith('MTD'))
-                    and fn.endswith('.xml')):
-                metadata_file = fn
-                metadata_path.append(
-                    os.path.join(os.path.dirname(imgFolder), fn))
-
-        if len(metadata_path) > 1:
-            message = (
-                'Make sure only the original metadata exists in the tile '
-                'folder\n{}\nAborting.'
-                ).format(os.path.dirname(imgFolder))
-            print message
-            logger.critical(message)
-            sys.exit()
+    for procFolder in procFolders:
 
         #
-        # Parse the metadata xml-file. There should only be one path.
+        # Initialize variables for PROC_DATA folder.
         #
-        try:
-            tree = etree.parse(metadata_path[0])
-        except Exception as e:
-            message = (
-                '{} {} in {} could not be parsed.'
-                ).format(str(e), metadata_path[0], imgFolder)
-            logger.critical(message)
+        caltembyt_path = None
+        calrefbyt_path = None
+        files_to_warp = []
 
-        for namespace in XML_namespaces:
+        for filename in os.listdir(procFolder):
 
-            try:
+            if filename.endswith('caltembyt_lndstlk.dat'):
 
-                #
-                # Get metadata values from the General_Info element.
-                #
-                General_Info = tree.find(
-                    '{' + namespace + 'PSD/'
-                    'S2_PDI_Level-1C_Tile_Metadata.xsd}General_Info')
-                TILE_ID = General_Info.find('TILE_ID').text
-                tile_id = TILE_ID[-12:-7]
-                SENSING_TIME = General_Info.find('SENSING_TIME').text
+                caltembyt_path = os.path.join(procFolder, filename)
+                files_to_warp.append(caltembyt_path)
 
-                #
-                # Get metadata values from the Geometric_Info element.
-                #
-                Geometric_Info = tree.find(
-                    '{' + namespace + 'PSD/'
-                    'S2_PDI_Level-1C_Tile_Metadata.xsd}Geometric_Info')
-                HORIZONTAL_CS_NAME = Geometric_Info.find(
-                    'Tile_Geocoding').find('HORIZONTAL_CS_NAME').text
-                HORIZONTAL_CS_CODE = Geometric_Info.find(
-                    'Tile_Geocoding').find('HORIZONTAL_CS_CODE').text
-                break
+            elif filename.endswith('calrefbyt_lndstlk.dat'):
 
-            except Exception as e:
-                message = ('{} {} in {} could not be parsed with {}.').format(
-                    str(e), metadata_path[0], imgFolder, namespace)
-                logger.error(message)
-        else:
-            message = ('{} in {} could not be parsed.').format(
-                metadata_path[0], imgFolder)
-            logger.error(message)
-            continue
-
-        tile_bands = []
-
-        #
-        # Retrieve desired bands from old data structure.
-        #
-        if metadata_file.startswith('S2A_'):
-            for dirpath, dirnames, filenames in os.walk(
-                    imgFolder, topdown=True):
-                for filename in filenames:
-                    if (filename.startswith('S2A')
-                            and filename.endswith('.jp2')
-                            and (fnmatch.fnmatch(filename, '*_B02.*')
-                                 or fnmatch.fnmatch(filename, '*_B03.*')
-                                 or fnmatch.fnmatch(filename, '*_B04.*')
-                                 or fnmatch.fnmatch(filename, '*_B08.*')
-                                 or fnmatch.fnmatch(filename, '*_B11.*')
-                                 or fnmatch.fnmatch(filename, '*_B12.*'))):
-
-                        tile_bands.append(os.path.join(dirpath, filename))
-
-        #
-        # Retrieve desired bands from data structure.
-        #
-        elif metadata_file.startswith('M'):
-            for dirpath, dirnames, filenames in os.walk(
-                    imgFolder, topdown=True):
-                for filename in filenames:
-                    if (filename.startswith('T')
-                            and filename.endswith('.jp2')
-                            and (fnmatch.fnmatch(filename, '*_B02.*')
-                                 or fnmatch.fnmatch(filename, '*_B03.*')
-                                 or fnmatch.fnmatch(filename, '*_B04.*')
-                                 or fnmatch.fnmatch(filename, '*_B08.*')
-                                 or fnmatch.fnmatch(filename, '*_B11.*')
-                                 or fnmatch.fnmatch(filename, '*_B12.*'))):
-
-                        tile_bands.append(os.path.join(dirpath, filename))
-
-        #
-        # Put bands in numeric order for processing. Redundant, keep anyways.
-        #
-        tile_bands.sort()
+                calrefbyt_path = os.path.join(procFolder, filename)
+                files_to_warp.append(calrefbyt_path)
 
         warp_opts = gdal.WarpOptions(
-            format="GTiff",
+            format="ENVI",
             resampleAlg=gdal.GRIORA_Bilinear,
             dstSRS='EPSG:{}'.format(warp_epsg))
-
-        for band in tile_bands:
-
-            orginal_name = os.path.basename(band)[:-4]
-            warped_name = '{}{}'.format(orginal_name, '.tif')
-            warped_filepath = os.path.join(imgFolder, warped_name)
-            band_filepath = band
-
-            img = gdal.Open(band, gdal.GA_ReadOnly)
-            ds = gdal.Warp(warped_filepath, img, options=warp_opts)
-            img = None
-            ds = None
-
-            os.remove(band)
+        print files_to_warp
+        print 'next'
+        # for file in files_to_warp:
+        #
+        #     orginal_name = os.path.basename(file)
+        #     warped_name = '{}{}'.format(orginal_name[:-4], '_warp.dat')
+        #     warped_filepath = os.path.join(procFolder, warped_name)
+        #     original_filepath = file
+        #
+        #     img = gdal.Open(file, gdal.GA_ReadOnly)
+        #     ds = gdal.Warp(warped_filepath, img, options=warp_opts)
+        #     img = None
+        #     ds = None
+        #     os.remove(file)
+        #     os.rename(warped_filepath, original_filepath)
 
     print '\n\n==============================================================='
     print 'Done processing.'
