e067127aaebb43f23008dcb973d4acf661c8f04c
Hannah <hannah.augustin@gmail.com>
2019-01-23 11:20:25 +0100

    add 8bit option to collect proc folders

--- thesis/warp_linux.py
+++ thesis/warp_linux.py
@@ -107,6 +107,9 @@ def get_args():
             '-wa', '--warp', dest='warp', action='store', type=str,
             help='EPSG code for warping.', default=None)
         parser.add_argument(
+            '-b8', '--bit8', dest='bit8', action='store', type=str,
+            help='If 8bit is to be warped.', default=None)
+        parser.add_argument(
             '--auto', dest='auto', action='store',
             help=('No user input necessary -- automatically converts all '
                   'previously not converted images files.'),
@@ -405,6 +408,283 @@ def warp_imgs(root_folder, imgFolders, warp_epsg):
         datetime.datetime.now() - start_time)
     logger.info(message)
 
+def get_procFolders(options_in):
+
+    #
+    # Create list for IMG_DATA folder and existing PROC_DATA folder paths.
+    #
+    procFolders = []
+
+    for dirpath, dirnames, filenames in os.walk(
+            options_in.read_dir, topdown=True):
+
+        for dirname in dirnames:
+
+            if dirname == 'PROC_DATA':
+
+                procFolder = os.path.join(dirpath, dirname)
+
+                procFolders.append(procFolder)
+    #
+    # Determine which tile folders have calrefbyt files.
+    #
+    unprocFolders = []
+
+    #
+    # Check validity of relevant PROC_DATA contents.
+    #
+    for procFolder in procFolders:
+
+        #
+        # Initialize variables for PROC_DATA folder.
+        #
+        caltembyt_path = None
+        caltembyt_size = 0
+        calrefbyt_path = None
+        calrefbyt_size = 0
+        keep_procFolder_1 = None
+        keep_procFolder_2 = None
+
+        for filename in os.listdir(procFolder):
+
+            if filename.endswith('caltembyt_lndstlk.dat'):
+
+                caltembyt_path = os.path.join(procFolder, filename)
+                caltembyt_size = os.path.getsize(caltembyt_path)
+
+                if caltembyt_size > 5:
+                    keep_procFolder_1 = True
+
+            elif filename.endswith('calrefbyt_lndstlk.dat'):
+
+                calrefbyt_path = os.path.join(procFolder, filename)
+                calrefbyt_size = os.path.getsize(calrefbyt_path)
+
+                if calrefbyt_size > 5:
+                    keep_procFolder_2 = True
+
+        #
+        # Removes empty PROC_DATA folders with problem files and adds to list to be
+        # processed.
+        #
+        if keep_procFolder_1 is True and keep_procFolder_2 is True:
+
+            unprocFolders.append(procFolder)
+
+        else:
+
+            shutil.rmtree(procFolder)
+
+    #
+    # Create the content of the popup window.
+    #
+    question = (
+        'Number of proc tiles found: {}'
+        '\n\nDo you want to process all unprocessed folders [{}]?'
+        ).format(len(procFolders), len(unprocFolders))
+
+    print question
+
+    ins = None
+    bool_ans = None
+
+    if options_in.auto is not None:
+
+        ins = 'y'
+
+    else:
+
+        while True:
+
+            ins = raw_input('Answer [y/n]: ')
+
+            if ins == 'y' or ins == 'n':
+
+                break
+
+            else:
+
+                print 'Your input should indicate yes [y] or no [n].'
+
+    if ins == 'y' or ins == 'Y' or ins == 'yes' or ins == 'Yes':
+
+        bool_ans = True
+
+    return bool_ans, unprocFolders
+
+def warp_envi(root_folder, imgFolders, warp_epsg):
+
+    start_time = datetime.datetime.now()
+
+    print '================================================================='
+    print 'Hold on to your hat. This may take ~45s per S2 tile folder.'
+    print 'Number of unprocessed PROC_DATA folders found: {}'.format(
+        len(imgFolders))
+    print 'Estimated time: {} minutes'.format(int(len(imgFolders)) * 0.75)
+    print 'Start time: {}'.format(start_time.time())
+    print '=================================================================\n'
+
+    message = (
+        'Root Folder: {} \nNumber of unprocessed IMG_DATA folders '
+        'found: {}\nStart time: {}'
+        ).format(root_folder, len(imgFolders), start_time.time())
+    logger.info(message)
+    #
+    # Register all of the GDAL drivers.
+    #
+    gdal.AllRegister()
+
+    #
+    # Possible XML Schema namespaces (plus a few potential future ones.)
+    #
+    XML_namespaces = ['https://psd-14.sentinel2.eo.esa.int/',
+                      'https://psd-12.sentinel2.eo.esa.int/',
+                      'https://psd-13.sentinel2.eo.esa.int/',
+                      'https://psd-15.sentinel2.eo.esa.int/',
+                      'https://psd-16.sentinel2.eo.esa.int/']
+
+    i = 0
+
+    for imgFolder in imgFolders:
+
+        metadata_path = []
+
+        for fn in os.listdir(os.path.dirname(imgFolder)):
+            if ((fn.startswith('S2A_') or fn.startswith('MTD'))
+                    and fn.endswith('.xml')):
+                metadata_file = fn
+                metadata_path.append(
+                    os.path.join(os.path.dirname(imgFolder), fn))
+
+        if len(metadata_path) > 1:
+            message = (
+                'Make sure only the original metadata exists in the tile '
+                'folder\n{}\nAborting.'
+                ).format(os.path.dirname(imgFolder))
+            print message
+            logger.critical(message)
+            sys.exit()
+
+        #
+        # Parse the metadata xml-file. There should only be one path.
+        #
+        try:
+            tree = etree.parse(metadata_path[0])
+        except Exception as e:
+            message = (
+                '{} {} in {} could not be parsed.'
+                ).format(str(e), metadata_path[0], imgFolder)
+            logger.critical(message)
+
+        for namespace in XML_namespaces:
+
+            try:
+
+                #
+                # Get metadata values from the General_Info element.
+                #
+                General_Info = tree.find(
+                    '{' + namespace + 'PSD/'
+                    'S2_PDI_Level-1C_Tile_Metadata.xsd}General_Info')
+                TILE_ID = General_Info.find('TILE_ID').text
+                tile_id = TILE_ID[-12:-7]
+                SENSING_TIME = General_Info.find('SENSING_TIME').text
+
+                #
+                # Get metadata values from the Geometric_Info element.
+                #
+                Geometric_Info = tree.find(
+                    '{' + namespace + 'PSD/'
+                    'S2_PDI_Level-1C_Tile_Metadata.xsd}Geometric_Info')
+                HORIZONTAL_CS_NAME = Geometric_Info.find(
+                    'Tile_Geocoding').find('HORIZONTAL_CS_NAME').text
+                HORIZONTAL_CS_CODE = Geometric_Info.find(
+                    'Tile_Geocoding').find('HORIZONTAL_CS_CODE').text
+                break
+
+            except Exception as e:
+                message = ('{} {} in {} could not be parsed with {}.').format(
+                    str(e), metadata_path[0], imgFolder, namespace)
+                logger.error(message)
+        else:
+            message = ('{} in {} could not be parsed.').format(
+                metadata_path[0], imgFolder)
+            logger.error(message)
+            continue
+
+        tile_bands = []
+
+        #
+        # Retrieve desired bands from old data structure.
+        #
+        if metadata_file.startswith('S2A_'):
+            for dirpath, dirnames, filenames in os.walk(
+                    imgFolder, topdown=True):
+                for filename in filenames:
+                    if (filename.startswith('S2A')
+                            and filename.endswith('.jp2')
+                            and (fnmatch.fnmatch(filename, '*_B02.*')
+                                 or fnmatch.fnmatch(filename, '*_B03.*')
+                                 or fnmatch.fnmatch(filename, '*_B04.*')
+                                 or fnmatch.fnmatch(filename, '*_B08.*')
+                                 or fnmatch.fnmatch(filename, '*_B11.*')
+                                 or fnmatch.fnmatch(filename, '*_B12.*'))):
+
+                        tile_bands.append(os.path.join(dirpath, filename))
+
+        #
+        # Retrieve desired bands from data structure.
+        #
+        elif metadata_file.startswith('M'):
+            for dirpath, dirnames, filenames in os.walk(
+                    imgFolder, topdown=True):
+                for filename in filenames:
+                    if (filename.startswith('T')
+                            and filename.endswith('.jp2')
+                            and (fnmatch.fnmatch(filename, '*_B02.*')
+                                 or fnmatch.fnmatch(filename, '*_B03.*')
+                                 or fnmatch.fnmatch(filename, '*_B04.*')
+                                 or fnmatch.fnmatch(filename, '*_B08.*')
+                                 or fnmatch.fnmatch(filename, '*_B11.*')
+                                 or fnmatch.fnmatch(filename, '*_B12.*'))):
+
+                        tile_bands.append(os.path.join(dirpath, filename))
+
+        #
+        # Put bands in numeric order for processing. Redundant, keep anyways.
+        #
+        tile_bands.sort()
+
+        warp_opts = gdal.WarpOptions(
+            format="GTiff",
+            resampleAlg=gdal.GRIORA_Bilinear,
+            dstSRS='EPSG:{}'.format(warp_epsg))
+
+        for band in tile_bands:
+
+            orginal_name = os.path.basename(band)[:-4]
+            warped_name = '{}{}'.format(orginal_name, '.tif')
+            warped_filepath = os.path.join(imgFolder, warped_name)
+            band_filepath = band
+
+            img = gdal.Open(band, gdal.GA_ReadOnly)
+            ds = gdal.Warp(warped_filepath, img, options=warp_opts)
+            img = None
+            ds = None
+
+            os.remove(band)
+
+    print '\n\n==============================================================='
+    print 'Done processing.'
+    print 'End time: {}'.format(datetime.datetime.now().time())
+    print 'Total elapsed time: {}'.format(datetime.datetime.now() - start_time)
+    print '===============================================================\n\n'
+    message = ('End time: {}').format(datetime.datetime.now().time())
+    logger.info(message)
+    message = ('Total elapsed time: {}').format(
+        datetime.datetime.now() - start_time)
+    logger.info(message)
+
 
 if __name__ == '__main__':
 
@@ -422,17 +702,39 @@ if __name__ == '__main__':
     options = get_args()
     root_folder = options.read_dir
 
-    bool_answer, imgFolders_toProcess = check_imgFolders(options)
+    if options.bit8 == None:
 
-    if not bool_answer:
-        print 'No folders processed.'
-        sys.exit(1)
+        bool_answer, imgFolders_toProcess = check_imgFolders(options)
 
-    if imgFolders_toProcess == 0:
+        if not bool_answer:
+            print 'No folders processed.'
+            sys.exit(1)
 
-        print 'No new folders to process.'
-        logger.info('No new folders to process.')
+        if imgFolders_toProcess == 0:
+
+            print 'No new folders to process.'
+            logger.info('No new folders to process.')
+
+        else:
+
+            warp_imgs(root_folder, imgFolders_toProcess, options.warp)
+
+    elif options.bit8 == 'y':
+
+        bool_answer, procFolders_toProcess = get_procFolders(options)
+
+        if not bool_answer:
+            print 'No folders processed.'
+            sys.exit(1)
+
+        if procFolders_toProcess == 0:
+
+            print 'No new folders to process.'
+            logger.info('No new folders to process.')
+
+        else:
+            print procFolders_toProcess
+            # warp_envi(root_folder, procFolders_toProcess, options.warp)
 
     else:
-
-        warp_imgs(root_folder, imgFolders_toProcess, options.warp)
+        print 'Unknown command. Try again.'
