b78c928eee3fe94445f0ac1235045862b4c6b212
augustinh22 <hannah.augustin@gmail.com>
2019-10-23 18:40:47 +0200

    add 37SBU and 37SBV

--- thesis/automate_downloads.sh
+++ thesis/automate_downloads.sh
@@ -5,10 +5,14 @@ cd /home/hannah/repos/AIQ/thesis/
 python download_linux.py -t 37SBA --auto y -a ./key.txt -w /data/s2/37SBA
 python download_linux.py -t 37SCA --auto y -a ./key.txt -w /data/s2/37SCA
 python download_linux.py -t 37SDA --auto y -a ./key.txt -w /data/s2/37SDA
+python download_linux.py -t 37SBU --auto y -a ./key.txt -w /data/s2/37SBU
+python download_linux.py -t 37SBV --auto y -a ./key.txt -w /data/s2/37SBV
 
 python conversion_linux.py --auto y -r /data/s2/37SBA
 python conversion_linux.py --auto y -r /data/s2/37SCA
 python conversion_linux.py --auto y -r /data/s2/37SDA
+python conversion_linux.py --auto y -r /data/s2/37SBU
+python conversion_linux.py --auto y -r /data/s2/37SBV
 
 python batch_linux.py --auto y -r /data/s2/
 cd /home/hannah/repos/AIQ/thesis/siam/
